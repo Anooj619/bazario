d13c8a993118582cb4d066c700d77b8d372610fd
Anooj V <anoojv619@gmail.com>
2025-09-01 15:09:29 +0530

    url change.

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://bazario-v8i7.onrender.com/api',
-  imageBaseUrl: 'https://anooj619.github.io/'
+  imageBaseUrl: 'https://anooj619.github.io'
 };
